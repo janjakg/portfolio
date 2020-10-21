60addec96a0358d47a95114f6661a7bdd2b77adf
janjakg <janjakg@yahoo.fr>
2020-10-21 17:05:32 +0200

    creation du portfolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#phone').click(function() {
+        $('#number-phone').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTON MON NUMERO
+    });
+});
+$(function() {
+    $('#email').click(function() {
+        $('#e-mail').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTON MON EMAIL
+    });
+});
+$(function() {
+    $('#linkedin').click(function() {
+        $('#linked-in').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTON MON ADRESSE LINKEDIN
+    });
+});
